abd591a0f01fec8f9c2c6159742dda77cc5e338d
Robert Finch <5563401+robfinch@users.noreply.github.com>
2024-08-12 15:07:46 -0400

    inc - update
    - FTA bus doc

---
+++ const_pkg.sv
@@ -0,0 +1,17 @@
+package const_pkg;
+
+`define TRUE	1'b1
+`define FALSE	1'b0
+`define HIGH	1'b1
+`define LOW		1'b0
+`define VAL		1'b1
+`define INV		1'b0
+
+ parameter TRUE = `TRUE;
+ parameter FALSE = `FALSE;
+ parameter VAL = `VAL;
+ parameter INV = `INV;
+ parameter HIGH = `HIGH;
+ parameter LOW = `LOW;
+
+endpackage
